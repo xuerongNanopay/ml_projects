a3400ca013860ad2d7b42b63d268320c14db25aa
xuerongNanopay <xuerong@nanopay.net>
2024-03-26 23:43:13 -0400

    rename model to unet

---
+++ unet_pytorch/dataset.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+
+class CarvanaDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index])
