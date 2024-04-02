ca326e0c35ebc66b2d6d35053248b1cf2edb2a9d
xuerongNanopay <xuerong@nanopay.net>
2024-04-02 17:39:55 -0400

    add example for model persistance

--- pytorch/model_persistance.py
+++ pytorch/model_persistance.py
@@ -5,39 +5,40 @@ import torch.nn.functional as F  # Active Function, Relu
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("DEVICE: ", device)
 
 # Hyperparameters
-input_size = 28
-sequence_length = 28
-num_layers = 2
-hidden_size = 256
+input_size = 28 * 28
 num_classes = 10
-batch_size = 64
-num_epochs = 4
 learning_rate = 0.001
+batch_size = 64
+num_epochs = 2
+load_model = True
 
-# Create a bidirectional LSTM.
-class BRNN(nn.Module):
+class CNN(nn.Module):
     """
-        Bi-Direction LSTM model for MNIST.
-        Important: treat image as 28 sequence and 28 features.
+        CNN model for MNIST
     """
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(BRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(hidden_size*2, num_classes)
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+        # n_out = (n_in + 2*padding - kernel)/stride + 1
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3, 3), stride=(1, 1),
+                               padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16 * 7 * 7, num_classes)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
-        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        # reshape
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
 
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
-        return out
 def check_accuracy(loader, model):
     if loader.dataset.train:
         print("Checking accuracy on training data")
@@ -49,7 +50,7 @@ def check_accuracy(loader, model):
 
     with torch.no_grad():
         for x, y in loader:
-            x = x.to(device=device).squeeze(1)
+            x = x.to(device=device)
             y = y.to(device=device)
 
             scores = model(x)  # 64 * 10
@@ -60,6 +61,15 @@ def check_accuracy(loader, model):
 
     model.train()
 
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
 # Load Data
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
@@ -67,17 +77,28 @@ test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 # Initialize network
-model = BRNN(input_size, hidden_size, num_layers, num_classes).to(device)
+model = CNN().to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
 # Train Network
 for epoch in range(num_epochs):
+    losses = []
+
+    if epoch % 2 == 0:
+        checkpoint = {
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        }
+        save_checkpoint(checkpoint)
+
     for batch_idx, (data, targets) in enumerate(train_loader):
-        # Make sure data
-        data = data.to(device=device).squeeze(1)
+        data = data.to(device=device)
         targets = targets.to(device=device)
 
         # forward
@@ -95,6 +116,7 @@ for epoch in range(num_epochs):
     check_accuracy(train_loader, model)
     check_accuracy(test_loader, model)
 
+# Check accuracy on training & test to see how good our model.
 
 if __name__ == "__main__":
-    print("dBiLSTM Done")
+    print("🇨🇦CNN Model Persistance Done")
