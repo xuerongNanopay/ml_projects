60203d48cdeaff6ce4515a050a012992da2316cb
xuerongNanopay <xuerong@nanopay.net>
2024-04-02 23:34:33 -0400

    add skeleton

---
+++ pytorch/custom_dataset.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import torch
+import torch.utils.data as Dataset
+import skimage as io
+
+class CatsAndDogsDataset(Dataset):
+    def __init__(self, test_dir, train_dir, transform=None):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
