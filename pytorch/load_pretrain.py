5255bf34943b77a4ec4b1a85bcc5c0087f953af2
xuerongNanopay <xuerong@nanopay.net>
2024-04-02 22:24:21 -0400

    load pretrain model

---
+++ pytorch/load_pretrain.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim  # Adam, SGD
+import torch.nn.functional as F  # Active Function, Relu
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
--- pytorch/model_persistance.py
+++ pytorch/model_persistance.py
@@ -4,40 +4,34 @@ import torch.optim as optim  # Adam, SGD
 import torch.nn.functional as F  # Active Function, Relu
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import torchvision
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparameters
-input_size = 28 * 28
+in_channel = 3
 num_classes = 10
 learning_rate = 0.001
-batch_size = 64
-num_epochs = 2
-load_model = True
+batch_size = 1024
+num_epochs = 5
 
-class CNN(nn.Module):
-    """
-        CNN model for MNIST
-    """
-    def __init__(self, in_channels=1, num_classes=10):
-        super(CNN, self).__init__()
-        # n_out = (n_in + 2*padding - kernel)/stride + 1
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3, 3), stride=(1, 1),
-                               padding=(1, 1))
-        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        self.fc1 = nn.Linear(16 * 7 * 7, num_classes)
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        # reshape
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
         return x
+
+model = torchvision.models.vgg16(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.avgpool = Identity()
+model.classifier = nn.Sequential(nn.Linear(512, 100), nn.ReLU(), nn.Linear(100, 10))
 
 def check_accuracy(loader, model):
     if loader.dataset.train:
@@ -61,41 +55,19 @@ def check_accuracy(loader, model):
 
     model.train()
 
-def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
-    print("=> Saving checkpoint")
-    torch.save(state, filename)
-
-def load_checkpoint(checkpoint):
-    print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
-
 # Load Data
-train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+train_dataset = datasets.CIFAR10(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.CIFAR10(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
-# Initialize network
-model = CNN().to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-if load_model:
-    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
-
 # Train Network
 for epoch in range(num_epochs):
-    losses = []
-
-    if epoch % 2 == 0:
-        checkpoint = {
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict()
-        }
-        save_checkpoint(checkpoint)
 
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device=device)
@@ -119,4 +91,4 @@ for epoch in range(num_epochs):
 # Check accuracy on training & test to see how good our model.
 
 if __name__ == "__main__":
-    print("🇨🇦CNN Model Persistance Done")
+    print("🇨🇦Preload Model Done")
