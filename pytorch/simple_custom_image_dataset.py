5d66447c42526bed31c416e3302f2d9bcc6ea829
xuerongNanopay <xuerong@nanopay.net>
2024-04-03 16:13:21 -0400

    example for loading custom image

---
+++ pytorch/simple_custom_image_dataset.py
@@ -0,0 +1,63 @@
+import torch
+import numpy as np
+import cv2
+from PIL import Image
+import torch.nn as nn
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import os
+
+class ImageFolder(nn.Module):
+    def __init__(self, root_dir, transform=None):
+        super(ImageFolder, self).__init__()
+        self.data= []
+        self.root_dir = root_dir
+        self.transform = transform
+        self.class_names = os.listdir(root_dir)
+
+        for index, name in enumerate(self.class_names):
+            files = os.listdir(os.path.join(root_dir, name))
+            files = list(filter(lambda x: x.endswith(".jpg"), files))
+            self.data += list(zip(files, [index]*len(files)))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        ima_files, label = self.data[index]
+        root_and_dir = os.path.join(self.root_dir, self.class_names[label])
+        image = np.array(Image.open(os.path.join(root_and_dir, ima_files)))
+
+        if self.transform is not None:
+            augmentation = self.transform(image=image)
+            image = augmentation['image']
+        return image, label
+
+transform = A.Compose(
+    [
+        A.Resize(width=1920, height=1080),
+        A.RandomCrop(width=1280, height=720),
+        A.Rotate(limit=40, p=0.9),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.1),
+        A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
+        A.OneOf([
+            A.Blur(blur_limit=3, p=0.5),
+            A.ColorJitter(p=0.5)
+        ], p=1.0),
+        A.Normalize(
+            mean=[0, 0, 0],
+            std=[1, 1, 1],
+            max_pixel_value=255
+        ),
+        ToTensorV2(),
+    ]
+)
+
+dataset = ImageFolder(root_dir="dataset/cat_and_dog/", transform=transform)
+
+for x, y in dataset:
+    print(x.shape)
+
+if __name__ == "__main__":
+    print("🇨🇦Simple Custom Image Dataset Done")
